e01c2fadd37ddff71c90ce6db8cb1f697f6ecb1a
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2022-09-02 15:51:01 +0700

    add swap test

--- contracts/swaprouter/tests/swap_test.rs
+++ contracts/swaprouter/tests/swap_test.rs
@@ -2,7 +2,7 @@ mod common;
 use common::*;
 use cosmwasm_std::Coin;
 use osmosis_std::types::osmosis::gamm::v1beta1::SwapAmountInRoute;
-use osmosis_testing::account::Account;
+use osmosis_testing::{account::Account, app::App};
 use swaprouter::msg::ExecuteMsg;
 
 test_swap!(
@@ -82,26 +82,32 @@ macro_rules! test_swap {
     ($test_name:ident should succeed, msg = $msg:expr, funds: $funds:expr) => {
         #[test]
         fn $test_name() {
-            let res = setup_and_swap(&$msg, &$funds);
+            let res = setup_and_swap(&$msg, &$funds, &check_input_decreased_and_output_increased);
             assert!(res.is_ok(), "{}", res.unwrap_err());
         }
     };
     ($test_name:ident should failed_with $err:expr, msg = $msg:expr, funds: $funds:expr) => {
         #[test]
         fn $test_name() {
-            let res = setup_and_swap(&$msg, &$funds);
+            let res = setup_and_swap(&$msg, &$funds, &|_, _, _| {});
             assert_eq!(res.unwrap_err(), $err);
         }
     };
 }
 
-fn setup_and_swap(msg: &ExecuteMsg, funds: &[Coin]) -> Result<String, String> {
+const INITIAL_AMOUNT: u128 = 1_000_000_000_000;
+
+fn setup_and_swap(
+    msg: &ExecuteMsg,
+    funds: &[Coin],
+    check: &dyn Fn(&App, &str, &ExecuteMsg),
+) -> Result<String, String> {
     let (app, contract_address, owner) = setup_test_env();
 
     let initial_balance = [
-        Coin::new(1_000_000_000_000, "uosmo"),
-        Coin::new(1_000_000_000_000, "uion"),
-        Coin::new(1_000_000_000_000, "uatom"),
+        Coin::new(INITIAL_AMOUNT, "uosmo"),
+        Coin::new(INITIAL_AMOUNT, "uion"),
+        Coin::new(INITIAL_AMOUNT, "uatom"),
     ];
 
     let non_owner = app.init_account(&initial_balance);
@@ -122,5 +128,38 @@ fn setup_and_swap(msg: &ExecuteMsg, funds: &[Coin]) -> Result<String, String> {
     app.execute_contract(&owner.address(), &contract_address, &set_route_msg, &[])
         .expect("Setup route fixture must always succeed");
 
-    app.execute_contract(&non_owner.address(), &contract_address, &msg, funds)
+    let res = app.execute_contract(&non_owner.address(), &contract_address, &msg, funds);
+
+    check(&app, &non_owner.address(), msg);
+    res
+}
+
+fn check_input_decreased_and_output_increased(app: &App, sender: &str, msg: &ExecuteMsg) {
+    let balances = app.get_all_balances(sender);
+    if let ExecuteMsg::Swap {
+        input_coin,
+        output_denom,
+        ..
+    } = msg
+    {
+        let input = balances
+            .iter()
+            .find(|b| b.denom == input_coin.denom)
+            .unwrap();
+        let output = balances
+            .iter()
+            .find(|b| *b.denom == *output_denom.as_str())
+            .unwrap();
+
+        assert!(
+            input.amount < INITIAL_AMOUNT.into(),
+            "Input must be decreased after swap"
+        );
+        assert!(
+            output.amount > INITIAL_AMOUNT.into(),
+            "Output must be increased after swap"
+        );
+    } else {
+        panic!("Wrong message type: Must be `ExecuteMsg::Swap`");
+    }
 }
